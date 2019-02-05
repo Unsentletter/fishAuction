df3317befcf10365d12595e223680d9f21e25dc7
Tristan Southwell <tristan.southwell@hotmail.com>
2019-02-05 22:36:51 +1100

    Knex implemented

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,8 +1,12 @@
-module.exports = {
+import Knex from "knex";
+export const db = Knex({
   client: 'mysql',
   connection: {
     user: 'root',
     password: 'password',
     database: 'fish_auction'
   }
-};
+});
+
+
+// db('tableName').select('col1', 'col2').where('col1', 'test')
