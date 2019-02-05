a9ce4ae8ef0f33d796d76a0d4a5b617788493e31
Tristan Southwell <tristan.southwell@hotmail.com>
2019-02-05 22:35:28 +1100

    Auth middleware reinstated

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,28 +1,28 @@
-import knex from 'knex';
+import { db } from '../knexfile';
 import jwt from 'jsonwebtoken';
 import {Request, Response, NextFunction} from "express";
 
-
 // TODO - Redo any type
-// export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-//   const token: any = req.headers['token'];
-//   console.log("Auth middleware", token);
-//
-//   if(!token) {
-//     console.log("No token");
-//     return res
-//       .status(400)
-//       .send({ 'message': 'Token is not provided' });
-//   }
-//
-//   try {
-//     const decoded = await jwt.verify(token, 'abc123');
-//     const user = await knex('user').where({id: decoded});
-//
-//     req.body.user = { user };
-//     next();
-//   } catch(error) {
-//     return res.status(400).send(error);
-//   }
-//   next()
-// };
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+  const token: any = req.headers['token'];
+  console.log("Auth middleware", token);
+
+  if(!token) {
+    console.log("No token");
+    return res
+      .status(400)
+      .send({ 'message': 'Token is not provided' });
+  }
+
+  try {
+    const decoded: any = await jwt.verify(token, 'abc123');
+    console.log(1111111111, decoded.id);
+    const user = await db('user').where({id: decoded.id});
+
+    req.body.user = { user };
+    next();
+  } catch(error) {
+    return res.status(400).send(error);
+  }
+  next()
+};
